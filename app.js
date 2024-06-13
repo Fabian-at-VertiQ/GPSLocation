a0cad736efe85a720a1884b192669be3ab3cd088
VertiQ - Fabian Valli <fabian@vertiq.com>
2024-06-13 11:50:34 -0700

    Update events call

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ document.addEventListener("DOMContentLoaded", (e)=>{
     const location = document.getElementById("Location");
     const lat = document.getElementById("lat");
     const clearAllWarning = document.getElementById("ClearAllWarning");
-    function showDlg() {
+    const clearData = document.getElementById("ClearData");
+    const hideWarning = document.getElementById("HideWarning");
+
+    clearData.addEventListener("click", (e) =>{
         clearAllWarning.showModal();
-    }
-    function hideDlg() {
+    });
+
+
+    hideWarning.addEventListener("click", (e) => {
         clearAllWarning.close();
-    }
+    });
 
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("sw.js");
